b02140cee3d27236401bee1c6a8a979345d485eb
Benjamin Siret <benjaminsiret@outlook.fr>
2023-03-03 14:34:19 +0100

    add footer, add some css

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,12 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 import logo from "../assets/logo.png";
+import logoBw from "../assets/logoBw.png";
 
 export default function Root() {
   return (
     <div id='root'>
-      <header>
+      <header id='header'>
         <nav className='navbar'>
           <img
             src={logo}
@@ -32,6 +33,14 @@ export default function Root() {
       <main>
         <Outlet />
       </main>
+      <footer id='footer'>
+        <img
+          src={logoBw}
+          alt='logo'
+          className='footer-logo'
+        />
+        <p className='footer-text'>© 2020 Kasa. All rights reserved</p>
+      </footer>
     </div>
   );
 }
