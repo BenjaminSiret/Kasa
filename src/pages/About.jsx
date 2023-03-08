0b03c1a4dbb55336444bb66a99be71c15b9e4617
Benjamin Siret <benjaminsiret@outlook.fr>
2023-03-08 17:13:11 +0100

    implement about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
+import chevron from "../assets/chevron.png";
+
 export default function About() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      <h2>Page à propos</h2>
+    <div className='about'>
+      <div className='dropdownContainer'>
+        <div className='dropdown-title'>
+          <h3 className='about-title'>Fiabilité</h3>
+          <img
+            src={chevron}
+            alt='chevron'
+            className={isOpen ? "chevron-open" : "chevron-close"}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+        <div className={isOpen ? "dropdown-open" : "dropdown-close"}>
+          <p className='about-text'>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
