c5c7814406569b3095ef8d2d0ab85313ae3202b1
Benjamin Siret <benjaminsiret@outlook.fr>
2023-03-10 13:51:01 +0100

    clean header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,13 @@ import logoBw from "../assets/logoBw.png";
 
 export default function Footer() {
   return (
-    <div>
-      <footer id='footer'>
-        <img
-          src={logoBw}
-          alt='logo'
-          className='footer-logo'
-        />
-        <p className='footer-text'>© 2020 Kasa. All rights reserved</p>
-      </footer>
-    </div>
+    <footer id='footer'>
+      <img
+        src={logoBw}
+        alt='logo'
+        className='footer-logo'
+      />
+      <p className='footer-text'>© 2020 Kasa. All rights reserved</p>
+    </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,31 +3,29 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <div>
-      <header id='header'>
-        <nav className='navbar'>
-          <img
-            src={logo}
-            alt='logo'
-          />
-          <div className='navLinks'>
-            <NavLink
-              to='/'
-              className='navLink'
-              // activeclassName='active'
-            >
-              Accueil
-            </NavLink>
-            <NavLink
-              to='about'
-              className='navLink'
-              // activeclassName='active'
-            >
-              A propos
-            </NavLink>
-          </div>
-        </nav>
-      </header>
-    </div>
+    <header id='header'>
+      <nav className='navbar'>
+        <img
+          src={logo}
+          alt='logo'
+        />
+        <div className='navLinks'>
+          <NavLink
+            to='/'
+            className='navLink'
+            // activeclassName='active'
+          >
+            Accueil
+          </NavLink>
+          <NavLink
+            to='about'
+            className='navLink'
+            // activeclassName='active'
+          >
+            A propos
+          </NavLink>
+        </div>
+      </nav>
+    </header>
   );
 }
